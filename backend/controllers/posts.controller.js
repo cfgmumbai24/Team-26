8f4c20b17a65850e0c3e64e8200e9f5508467005
Krishna251203 <krishna251203@gmail.com>
2024-06-15 23:46:34 +0530

    Add getPosts with filter

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -2,8 +2,8 @@ import Post from "../models/posts.model.js";
 
 export const createPost = async (req, res) => {
     try {
-        const { title, description, links } = req.body;
-        const post = new Post({ title, description, links });
+        const { title, description, links, tags } = req.body;
+        const post = new Post({ title, description, links, tags });
         if(post){
             await post.save();
             res.status(200).json({message:"Post created"})
@@ -12,4 +12,22 @@ export const createPost = async (req, res) => {
         console.log(`Error occured in createPost ${error}`);
     }
 
+}
+
+
+export const getPosts = async (req,res)=>{
+    try{
+    const tags = req.query.tags ? req.query.tags.split(',') : [];
+    const query = {};
+
+    if (tags && tags.length > 0) {
+        query.tags = { $in: tags }; // Match any of the tags
+    }
+
+    const posts = await Post.find(query).exec();
+    res.status(200).json(posts);
+            
+    }catch (error) {
+        console.log(`Error occured in getPosts ${error}`)
+    }
 }
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createPost } from '../controllers/posts.controller.js'
+import { createPost, getPosts } from '../controllers/posts.controller.js'
 
 const router = express.Router();
 
 router.post('/posts', createPost);
+router.post("/",getPosts);
 
 export default router;
